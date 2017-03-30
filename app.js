6a59ca7b8967328ee377d94856a34bfd823af56d
Jake <jake@Jakes-MacBook-Air.local>
2017-03-30 12:30:35 -0400

    Separated the functions into multiple files and changed the training function to only train on the top 5 most encountered words in an entry

--- app.js
+++ app.js
@@ -1,80 +1,39 @@
-var fs = require('fs');
-var xml2js = require('xml2js');
-var BrainJSClassifier = require('natural-brain');
+var fs = require('fs'); //Read and write to files
+var xml2js = require('xml2js'); //Convert XML to a JS object
+var parser = new xml2js.Parser(); //Parse XML
+var argv = require('yargs').argv; //To read arguments from the terminal
 
+//Neural Network and NLP imports
+var BrainJSClassifier = require('natural-brain'); 
 var classifier = new BrainJSClassifier();
-var parser = new xml2js.Parser();
 
+var nnFunctions = require("./neural_net_functions");
+
+//Location to save the classifier information learned
 const saveFile = __dirname + "/classifier.json";
 
-loadNeuralNetworkData("classifier.json", function(loadedClassifier){
-	classifier = loadedClassifier;
+var startTime = Date.now(); //Used for testing the training speeds
 
-	trainNeuralNetwork(__dirname + "/data/reut2-000.sgm", function(){
-		saveNeuralNetwork("classifier.json", function(result){
+// Load the classifier data from the data file
+nnFunctions.loadNeuralNetworkData("classifier.json", function(loadedClassifier){
+
+	//Set the classifier to that of the one loaded from the file
+	classifier = loadedClassifier;
+	console.log("It took " + (Date.now() - startTime) + "ms to load");
+
+	// Reset the start time for training
+	startTime = Date.now();
+
+	//Start training the neural network classifier using the referenced file
+	nnFunctions.trainNeuralNetwork(classifier, __dirname + "/data/" + (argv.file || 1) + ".sgm", function(resultClassifier){
+		//Update the classifier based on the recent training
+		classifier = resultClassifier;
+
+		console.log("It took " + ((Date.now() - startTime)/1000) + "s to train");
+		
+		//Save all of the training data to a file
+		nnFunctions.saveNeuralNetwork(classifier, "classifier.json", function(result){
 			console.log("Saved successfully");
 		});
 	});
-})
-
-//Load the neural network data from the save file
-function loadNeuralNetworkData(file, callback){
-	BrainJSClassifier.load(file, null, function(err, classifier) {
-		if(err) return console.log(err);
-		console.log("Loaded data from file...");
-		callback(classifier);
-	});
-}
-//Train the neural network using the specified file
-function trainNeuralNetwork(trainingTextFilename, callback){
-	//Read text from the file to train the neural network
-	fs.readFile(trainingTextFilename, function(err, data) {
-		
-		//Convert the XML to JSON
-		parser.parseString(data, function (err, result) {
-			
-			//Get all of the entries
-			var entries = result["DATA"]["REUTERS"];
-
-			console.log("Adding " + entries.length + " entries...");
-
-			//Add all entries to the neural network
-			for(var i = 0; i < 50; i++){
-
-				var currentEntry = entries[i];
-
-				//Get the important info from the entry
-				getInfoFromEntry(currentEntry, function(data){
-					if(data.topics !== undefined && data.body !== undefined){
-						classifier.addDocument(data.body, data.topics[0]);
-					}
-				});
-			}
-
-			console.log("Starting to train...");
-			classifier.train();
-			console.log("Done training.");
-
-			callback();
-		});
-	});
-}
-//Save the current neural network state to a file
-function saveNeuralNetwork(file, callback){
-	classifier.save(file, function(err, classifier) {
-		if(err) return console.log(err);
-		console.log("Neural Network training data saved to " + saveFile); 
-		callback(classifier);
-	});
-}
-//Return an object given one of the entries from a data file
-function getInfoFromEntry(entry, callback){
-	var topics = entry["TOPICS"][0]["D"] || undefined;
-	var textObj = entry["TEXT"][0];
-	var title = textObj["TITLE"];
-	var body = textObj["BODY"];
-
-	if(body !== undefined) body = body[0].replace(/(?:\r\n|\r|\n|\s+|a|the|and)/g, ' ');
-
-	callback({"body": body, "topics": topics });
-}
+});
---
+++ neural_net_functions.js
@@ -0,0 +1,119 @@
+var fs = require('fs'); //Read and write to files
+var xml2js = require('xml2js'); //Convert XML to a JS object
+var parser = new xml2js.Parser(); //Parse XML
+var argv = require('yargs').argv; //To read arguments from the terminal
+
+//Neural Network and NLP imports
+var BrainJSClassifier = require('natural-brain'); 
+var classifier = new BrainJSClassifier();
+
+//Return a classification for a referenced string using a trained classifier
+module.exports.classify = function (classifierToUse, stringToClassify){
+	return classifier.classify(stringToClassify);
+}
+//Returns all of the labels faced so far with their likelihood of 
+//matching the referenced string
+module.exports.getClassifications = function (classifierToUse, stringToClassify){
+	return classifierToUse.getClassifications(stringToClassify);
+}
+//Load the neural network data from the save file
+module.exports.loadNeuralNetworkData = function (file, callback){
+	BrainJSClassifier.load(file, null, function(err, classifier) {
+		if(err) return console.log(err);
+		console.log("Loaded data from file...");
+		callback(classifier);
+	});
+}
+//Train the neural network using the specified file
+module.exports.trainNeuralNetwork = function (classifierToTrain, trainingTextFilename, callback){
+	//Read text from the file to train the neural network
+	fs.readFile(trainingTextFilename, function(err, data) {
+		
+		//Convert the XML to JSON
+		parser.parseString(data, function (err, result) {
+			
+			//Get all of the entries
+			var entries = result["DATA"]["REUTERS"];
+
+			console.log("Adding " + entries.length + " entries...");
+
+			//Add all entries to the neural network
+			for(var i = 0; i < entries.length; i++){
+
+				var currentEntry = entries[i];
+
+				//Get the important info from the entry
+				module.exports.getInfoFromEntry(currentEntry, function(data){
+					if(data.topics !== undefined && data.body !== undefined){
+						console.log(data);
+						classifierToTrain.addDocument(data.body, data.topics[0]);
+					}
+				});
+			}
+
+			console.log("Starting to train...");
+
+			//Train the classifier with the added documents
+			classifierToTrain.train();
+
+			console.log("Done training.");
+
+			//Return the classifier trained with the new information
+			callback(classifierToTrain);
+		});
+	});
+}
+//Save the current neural network state to a file
+module.exports.saveNeuralNetwork = function (classifierToSave, file, callback){
+	classifierToSave.save(file, function(err, savedClassifier) {
+		if(err) return console.log(err);
+		console.log("Neural Network training data saved to " + saveFile); 
+		callback(savedClassifier);
+	});
+}
+//Return an object given one of the entries from a data file
+module.exports.getInfoFromEntry = function (entry, callback){
+	var topics = entry["TOPICS"][0]["D"] || undefined;
+	var textObj = entry["TEXT"][0];
+	var title = textObj["TITLE"];
+	var body = textObj["BODY"];
+
+	if(body !== undefined){ 
+		body = body[0].replace(/(?:\r\n|\r|\n|\s+|\sa\s|\sthe\s|\sand\s|\.|\')/g, ' ');
+
+		var words = body.split(" ");
+		var wordCounts = {};
+
+		for(var p = 0; p < words.length; p++){
+			var word = words[p].trim().toUpperCase();
+
+			if(word != "" && /^[a-zA-Z0-9]+$/i.test(word) && word != "THE" && word != "A" && word != "AND" && word != "REUTER"){
+				if(word in wordCounts){
+					wordCounts[word] = wordCounts[word] + 1;
+				}
+				else{
+					wordCounts[word] = 0;
+				}
+			}
+		}
+
+		var sortable = [];
+
+		for (var word in wordCounts) {
+			sortable.push([word, wordCounts[word]]);
+		}
+
+		var sortedValues = sortable.sort(function(a, b) {
+			return a[1] - b[1];
+		});
+
+		body = [];
+
+		//Only add the top 5 words
+		for(var p = 0; p < sortedValues.length && p < 5; p++){
+			body.push(sortedValues[p][0]);
+		}
+	}
+
+	callback({"body": body, "topics": topics });
+}
